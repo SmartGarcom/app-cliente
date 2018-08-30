e7f196d1f4e92a16fe35e37f506cb7cc7c9b9bf3
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2018-08-30 18:45:52 -0300

    Ajustes iniciais no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final routes = <String, WidgetBuilder>{
+  };
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Smart Garçom',
+      home: new MyHomePage(title: 'Smart Garçom'),
+      routes: routes,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+
+  static String tag = 'home-page';
+
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
