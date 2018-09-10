6d74e1137e127aaaa2813deb078640a30d2f7d06
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2018-09-10 10:26:51 -0300

    Desenhada tela de login

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_garcom/component/Buttons/roundedButton.dart';
+import 'package:smart_garcom/component/TextFields/ensure_visible_when_focused.dart';
+import 'package:smart_garcom/component/TextFields/inputField.dart';
 import 'package:smart_garcom/component/loading.dart';
 import 'package:smart_garcom/theme/style.dart';
 
@@ -12,29 +14,48 @@ class LoginScreen extends StatefulWidget {
   LoginScreenState createState() => new LoginScreenState();
 }
 
-enum Steps { LOADING, LOGIN }
+enum Steps { INICIO, LOADING, LOGIN }
+
 Steps step;
 
 class LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  FocusNode _focusNodeEmail = new FocusNode();
+  static final TextEditingController _emailController =
+      new TextEditingController();
+  FocusNode _focusNodePassword = new FocusNode();
+  static final TextEditingController _passwordController =
+      new TextEditingController();
+
+  ThemeData theme =
+      getTheme(brightness: Brightness.dark, primaryColor: Colors.teal);
 
   @override
   void initState() {
     super.initState();
-    step = Steps.LOADING;
+    step = Steps.INICIO;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: new Container(
-          padding: new EdgeInsets.all(24.0),
-          child: new Center(
+    return Theme(
+      data: theme,
+      child: new Scaffold(
+        backgroundColor: theme.primaryColor,
+        body: new Form(
+          key: _formKey,
+          child: new SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
+                /* -- Something large -- */
+                Container(
+                  width: double.infinity,
+                  height: 150.0,
+//                  color: Colors.red,
+                ),
                 new Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: new Icon(
@@ -45,6 +66,41 @@ class LoginScreenState extends State<LoginScreen> {
                 ),
 //                logo,
                 new SizedBox(height: 32.0),
+
+                new EnsureVisibleWhenFocused(
+                  focusNode: _focusNodeEmail,
+                  child: new TextFormField(
+                    decoration: const InputDecoration(
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      labelText: 'Email',
+                    ),
+                    onSaved: (String value) {
+                      //TODO
+                    },
+                    controller: _emailController,
+                    focusNode: _focusNodeEmail,
+                  ),
+                ),
+                new SizedBox(height: 32.0),
+
+                new EnsureVisibleWhenFocused(
+                  focusNode: _focusNodePassword,
+                  child: new TextFormField(
+                    decoration: const InputDecoration(
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      labelText: 'Senha',
+                    ),
+                    onSaved: (String value) {
+                      //TODO
+                    },
+                    controller: _passwordController,
+                    focusNode: _focusNodePassword,
+                  ),
+                ),
+                new SizedBox(height: 32.0),
+
                 step == Steps.LOADING
                     ? new Loading()
                     : new RoundedButton(
@@ -52,12 +108,16 @@ class LoginScreenState extends State<LoginScreen> {
                   borderWidth: 0.0,
                   buttonColor: colorInDark,
                   textColor: colorInLight,
-                  onTap: () {
-                  },
+                  onTap: () {},
                 ),
+
+
+
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
+ThemeData getTheme({Brightness brightness, Color primaryColor}) {
+  bool isLight = brightness == Brightness.light;
+  final Color accentColor = isLight ? primaryColor : Colors.white;
+  final _defaultShape = RoundedRectangleBorder(
+    borderRadius: new BorderRadius.all(new Radius.circular(16.0)),
+  );
+
+  return new ThemeData(
+    brightness: brightness,
+    primaryColor: primaryColor,
+//    fontFamily: 'Comfortaa',
+    buttonColor: accentColor,
+    buttonTheme: new ButtonThemeData(textTheme: ButtonTextTheme.primary),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(),
+      isCollapsed: true,
+      isDense: true,
+      filled: true,
+    ),
+    chipTheme: new ChipThemeData.fromDefaults(
+      brightness: brightness,
+      secondaryColor: accentColor,
+      labelStyle: new TextStyle(),
+    ).copyWith(
+      shape: _defaultShape,
+      backgroundColor: primaryColor.withOpacity(0.75),
+      brightness: brightness,
+      secondarySelectedColor: accentColor,
+    ),
+  );
+}
+
+
 TextStyle textStyle = const TextStyle(
   color: const Color(0XFFFFFFFF),
   fontSize: 16.0,
